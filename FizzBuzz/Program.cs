72fcca0568ba6e336723d56bde176f2c82570d89
Stine Linn Skjerven <stine_linn.skjerven@knowit.no>
2024-09-30 19:58:38 +0200

    Implement the game

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,4 +1,24 @@
-﻿Console.WriteLine("Hei, la oss spele FizzBuzz!");
+﻿using FizzBuzz;
+
+Console.WriteLine("Hei, la oss spele FizzBuzz!");
+
+var correct = true;
+var counter = 1;
+Console.WriteLine("Me startar på 1 og går til 100: ");
+
+while (correct)
+{
+    var input = Console.ReadLine();
+
+    var result = FizzBuzzService.FizzBuzzMethod(counter);
+    if (input != result)
+    {
+        Console.WriteLine($"Feil! Riktig svar er: {result}");
+        correct = false;
+    }
+
+    counter++;
+}
 
 Console.WriteLine("Trykk enter for å lukke applikasjonen");
 Console.ReadLine();
